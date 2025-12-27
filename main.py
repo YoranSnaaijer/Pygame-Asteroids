a50e26c16ebdd8be17974c90fe41f774b9eae084
github_username_here <email@example.com>
2025-12-27 21:13:42 +0100

    updated .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
+from logger import log_state
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(F"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 if __name__ == "__main__":
